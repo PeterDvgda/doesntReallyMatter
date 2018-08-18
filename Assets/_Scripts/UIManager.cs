1750bd13cb611e5bce0478769f91946e71eca726
kchang75 <kristi.chang@csu.fullerton.edu>
2018-08-18 11:16:18 -0700

    menu

--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -3,51 +3,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class UIManager : MonoBehaviour {
-    //Singleto instance of the UI manager
-    public static UIManager instance;
-    //Reference to the text to display the timer
-    public Text timerText;
+	//Singleton instance of the UI manager
+	public static UIManager instance;
+	//Reference to the text to display the timer
+	public Text timerText;
 
-    private void OnEnable()
-    {
-        //Set the singleton instance
-        if (instance == null)
-            instance = this;
-        //Subscribe to the OnUpdate event
-        GameManager.OnUpdate += OnUpdateHandler;
-        //Subscribe to the EndGame event
-        GameManager.OnEndGame += OnEndGameHandler;
-    }
-    private void OnDisable()
-    {
-        //Unsubscribe from the Update event
-        GameManager.OnUpdate -= OnUpdateHandler;
-        //Unsubscribe from the End Game event
-        GameManager.OnEndGame -= OnEndGameHandler;
-    }
+	private void OnEnable()
+	{
+		//Set the singleton instance
+		if (instance == null)
+			instance = this;
+		//Subscribe to the OnUpdate event
+		GameManager.OnUpdate += OnUpdateHandler;
+		//Subscribe to the EndGame event
+		GameManager.OnEndGame += OnEndGameHandler;
+	}
+	private void OnDisable()
+	{
+		//Unsubscribe from the Update event
+		GameManager.OnUpdate -= OnUpdateHandler;
+		//Unsubscribe from the End Game event
+		GameManager.OnEndGame -= OnEndGameHandler;
+	}
 
-    //Handler for when the End Game event is called
-    private void OnEndGameHandler()
-    {
-        Debug.Log("GAME END");
-    }
+	//Handler for when the End Game event is called
+	private void OnEndGameHandler()
+	{
+		Debug.Log("GAME END");
+	}
 
-    //Handler for when the Update event is called
-    private void OnUpdateHandler()
-    {
-    }
+	//Handler for when the Update event is called
+	private void OnUpdateHandler()
+	{
+	}
 
-    // Use this for initialization
-    void Start ()
-    {
+	// Use this for initialization
+	void Start ()
+	{
 
 	}
 
-    //Display the current time
-    public void DisplayTime(string timeString)
-    {
-        timerText.text = timeString;
-    }
+	//Display the current time
+	public void DisplayTime(string timeString)
+	{
+		timerText.text = timeString;
+	}
+
+
+	// Menu Functions
+	public void ClickedPlay()
+	{
+		SceneManager.LoadScene("TestScene");
+	}
+
+	public void ClickedQuit()
+	{
+		Application.Quit();
+	}
+
 	
 }
