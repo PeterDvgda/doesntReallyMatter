85d0efb0e29384c90714b7ed092db3d4e2725f84
Ken Miller <ken.miller521@gmail.com>
2018-08-19 11:12:44 -0700

    Revert "Merge branch 'master' of https://github.com/PeterDvgda/doesntReallyMatter"
    This reverts commit d2c7850e073d70322fbdc3bdc0825c5d75df8f7d, reversing
    changes made to 85d123c5644710bab1c9e036dc75ed49e74802e8.

--- Assets/_Scripts/CartSpawner.cs
+++ Assets/_Scripts/CartSpawner.cs
@@ -15,31 +15,20 @@ public class CartSpawner : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        // Spawn cart every 1 seconds, starting in 1 second
-        InvokeRepeating("Spawn", 1, 1);
+        // Spawn cart every 4 seconds, starting in 3
+        InvokeRepeating("Spawn", 3, 4);
     }
 
     // Spawn one cart
-    public void Spawn()
+    void Spawn()
     {
-        Collider2D[] results;
-
         // x position between left & right border
-        int x = (int)Random.Range(borderLeft.position.x + 2, borderRight.position.x - 2);
+        int x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
 
         // y position between top & bottom border
-        int y = (int)Random.Range(borderBottom.position.y + 2, borderTop.position.y - 2);
+        int y = (int)Random.Range(borderBottom.position.y, borderTop.position.y);
 
-        Vector2 spawnPoint = new Vector2(x, y);
-        if ()
-        {
-            Spawn();
-            Debug.Log("at point " + spawnPoint +" respawning");
-        }
-        else
-            Instantiate(cart, new Vector2(x, y), Quaternion.identity); // default rotation
-
-     
-
+        // Instantiate the cart at (x, y)
+        Instantiate(cart,new Vector2(x, y), Quaternion.identity); // default rotation
     }
 }
