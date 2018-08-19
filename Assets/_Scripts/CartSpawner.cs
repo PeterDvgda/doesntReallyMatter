6bdcfc2f94b76d3deef47207c01af7e150e6499e
Alex Pinedo <31834217+rocketmanhs@users.noreply.github.com>
2018-08-19 10:55:02 -0700

    Push

--- Assets/_Scripts/CartSpawner.cs
+++ Assets/_Scripts/CartSpawner.cs
@@ -15,20 +15,31 @@ public class CartSpawner : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        // Spawn cart every 4 seconds, starting in 3
-        InvokeRepeating("Spawn", 3, 4);
+        // Spawn cart every 1 seconds, starting in 1 second
+        InvokeRepeating("Spawn", 1, 1);
     }
 
     // Spawn one cart
-    void Spawn()
+    public void Spawn()
     {
+        Collider2D[] results;
+
         // x position between left & right border
-        int x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
+        int x = (int)Random.Range(borderLeft.position.x + 2, borderRight.position.x - 2);
 
         // y position between top & bottom border
-        int y = (int)Random.Range(borderBottom.position.y, borderTop.position.y);
+        int y = (int)Random.Range(borderBottom.position.y + 2, borderTop.position.y - 2);
 
-        // Instantiate the cart at (x, y)
-        Instantiate(cart,new Vector2(x, y), Quaternion.identity); // default rotation
+        Vector2 spawnPoint = new Vector2(x, y);
+        if ()
+        {
+            Spawn();
+            Debug.Log("at point " + spawnPoint +" respawning");
+        }
+        else
+            Instantiate(cart, new Vector2(x, y), Quaternion.identity); // default rotation
+
+     
+
     }
 }
